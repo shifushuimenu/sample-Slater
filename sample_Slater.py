sample via principal minors of exp(-X), sign problem not properly corrected, sampling thermal (!) FF density matrix does not work anymore (?)

sample_Slater.py
@@ -46,6 +46,7 @@ def Slater2spOBDM(U):
         OBDM[i,i] += 1.0
     return OBDM
 
+
 # @profile
 def sample_SlaterDeterminant(U, nu_rndvec):
     """
@@ -98,6 +99,155 @@ def sample_SlaterDeterminant(U, nu_rndvec):
     return occ_vec
 
 
+def sample_nonorthogonal_SlaterDeterminant(U, singular_values, Vh, nu_rndvec):
+    """
+        THIS DOES NOT WORK !
+        Input:
+            After singular value decomposition 
+                \exp(-X) = U @ S @ Vh
+            U:  Unitary matrix having left singular values as columns (Slater determinant no. 1)
+            singular_values: 
+            Vh: Unitary matrix having right singular vectors as rows (Slater determinant no. 2).  
+            nu_rndvec: A random permutation of the integers [1,2,...,N]
+        Output:
+            A Fock state of occupation numbers sampled from the input
+            Slater determinants. The Fock state is represented as a vector of
+            0s and 1s.
+    """
+    U = np.array(U, dtype=np.complex64)
+    Vh = np.array(Vh, dtype=np.complex64)
+    singular_values = np.array(singular_values, dtype=np.float32)
+    nu_rndvec = np.array(nu_rndvec, dtype=np.int32)
+
+    print("U.shape=", U.shape)
+    print("Vh.shape=", Vh.shape)
+    print("singular_values=", singular_values)
+
+    assert( U.shape[0] == Vh.shape[1] and U.shape[1] == Vh.shape[0] ), \
+        "shape mismatch: U: (%d, %d), Vh: (%d, %d)" % (U.shape + Vh.shape)
+
+    # M is the number of orbitals, N is the total number of particles 
+    (M,N) = U.shape
+    assert ( nu_rndvec.size == N ), "nu_rndvec.size = %d, M = %d" % (nu_rndvec.size, N)
+    assert ( singular_values.size == N )
+
+    # Occupation numbers of the M orbitals:
+    # occ_vec[i]==1 for occupied and occ_vec[i]==0 for unoccupied i-th orbital.
+    occ_vec = np.zeros(M, dtype=np.int8)
+
+    # Sample orbitals for N particles iteratively.
+    row_idx = []; col_idx = []
+    # unnormalized conditional probability cond_prob(x) for choosing orbital x for the
+    # n-th particle (The constant factor 1/(n!) in Ref. [1] can be neglected for the 
+    # unnormalized probability distribution.)
+    cond_prob = np.zeros(M, dtype=np.float32)
+    for n in range(N):
+        cond_prob[...] = 0.0
+        # collect row and column indices
+        col_idx = col_idx + list([nu_rndvec[n]])
+        for x_sample in range(M):
+            row_idx_sample = row_idx + list([x_sample])
+            assert (len(col_idx) == len(row_idx_sample))
+            # construct submatrix
+            Amat = U[np.ix_(row_idx_sample, col_idx)]
+            Bmat = Vh[np.ix_(col_idx, row_idx_sample)]
+            # product of singular values 
+            zz = np.prod(singular_values[col_idx])
+            xx = abs(linalg.det(Amat)*linalg.det(Bmat)) # avoid neg. prob. of the form -0.00000
+            # print("zz=", zz, "zz*xx=", zz*xx.real)
+            assert( xx.imag == 0 ), 'Product of determinants has imaginary part.'
+            cond_prob[x_sample] = zz * xx.real
+
+        # if (all(cond_prob < 1e-8)):
+        #     occ_vec[:] = 0
+        #     break
+
+        cumul_prob = prob2cumul(cond_prob)
+        x = bisection_search( prob=np.random.rand(), cumul_prob_vec=cumul_prob )
+        occ_vec[x] = 1
+        row_idx = row_idx + list([x])
+
+    return occ_vec
+
+
+def sample_expmX(U, N):
+    """
+        Input:
+            U = exp(-X), an M-by-M matrix representing the free fermion pseudo density matrix. 
+                exp(-X) is not necessarily hermitian.
+                The principal minors of exp(-X) are the marginal probabilities we need. 
+                They can be negative. 
+            N: Number of particles in the given particle number sector: N \in {0,1,...,D}.
+        Output:
+            A Fock state of occupation numbers sampled from the input free-fermion pseudo density matrix.
+            The Fock state carries a sign as well as a reweighting factor.
+    """
+    U = np.array(U, dtype=np.complex64)
+    assert(U.shape[0] == U.shape[1])
+    M = U.shape[0]
+    assert(N<=M)
+
+    # Occupation numbers of the M sites.
+    # occ_vec[i]==1 for occupied and occ_vec[i]==0 for unoccupied i-th site.
+    occ_vec = np.zeros(M, dtype=np.int8)
+    # If a conditional probability is negative, shift the sign to the observable,
+    # i.e. to the Fock state. 
+    sign_vec = np.zeros(M, dtype=np.int8)
+
+    # Sample sites for N particles iteratively.
+    row_idx = []; col_idx = []
+    # The conditional probability cond_prob(x) for choosing site x for the
+    # n-th particle is the ratio of two principal minors of exp(-X).
+    # Since this conditional probability can be negative for a pseudo density matrix,
+    # the absolute value is taken. 
+    cond_prob = np.zeros(M, dtype=np.float32)
+    cond_prob_sign = np.zeros(M, dtype=np.int8)
+    row_idx = []
+    # Sample the positions of N particles. 
+    for nn in range(N):
+        cond_prob[...] = 0.0
+        cond_prob_sign[...] = 0.0
+        # Collect row and column indices, which for a principal minor are the same,
+        # to obtain all elements of the conditional probability.
+        for x_sample in range(M):
+            row_idx_sample = row_idx + list([x_sample])
+            if (len(row_idx) >= 1):
+                # The conditional probability is given as the ratio of principal minors.
+
+                # Method 1: Low-rank update for iterative computation of the matrix inverse. 
+                # Use the block determinant formula, which allows to avoid the computation 
+                # of determinants altogether.
+                                                
+
+                # Method 2: Inverse of submatrix. Singular matrix for subsystem sizes larger than or equal to 5x5. 
+                # Add invertibility noise to make the matrix non-singular. 
+                v1 = U[np.ix_(list([x_sample]), row_idx)]
+                v2 = U[np.ix_(row_idx, list([x_sample]))]
+                dim = len(row_idx)
+                invertibility_noise = (1e-9)*np.random.random((dim,dim))
+                xx = U[x_sample, x_sample] - np.matmul(v1, np.matmul( linalg.inv(U[np.ix_(row_idx, row_idx)] + invertibility_noise), v2))
+
+                # Method 3: Ratio of two determinants. Leads to numerical overflow of the extremely large determinants.
+                #xx = linalg.det(U[np.ix_(row_idx_sample, row_idx_sample)]) / linalg.det(U[np.ix_(row_idx, row_idx)])
+            else:
+                # Determinant of a 1x1 matrix. 
+                xx = U[np.ix_(row_idx_sample, row_idx_sample)]    
+                # assert(xx > 0), "sign problem."
+            assert( xx.imag == 0 ), 'Something is wrong with the determinant. (%15.10f, %15.10f)' % (xx.real, xx.imag)
+            cond_prob[x_sample] = abs(xx.real)
+            cond_prob_sign[x_sample] = np.sign(xx.real)
+
+        cumul_prob = prob2cumul(cond_prob)
+        x = bisection_search( prob=np.random.rand(), cumul_prob_vec=cumul_prob )
+        occ_vec[x] = 1
+        sign_vec[x] = cond_prob_sign[x]
+        row_idx = row_idx + list([x])
+        col_idx = row_idx
+
+    return occ_vec, sign_vec
+
+
+
 def prob2cumul( prob_vec ):
     """
         For a vector of unnormalized probabilities, return a vector
@@ -134,7 +284,7 @@ def bisection_search( prob, cumul_prob_vec ):
     # TEST
 
     assert( all( cumul_prob_vec >= -np.finfo(float).eps ) ), print("cumul_prob_vec=", cumul_prob_vec)
-    assert( cumul_prob_vec[-1] == 1.0 )
+    assert( cumul_prob_vec[-1] == 1.0 ), "cumul_prob_vec[-1]=%15.10f" % cumul_prob_vec[-1]
 
     N = cumul_prob_vec.size
 

sample_free_fermion_OBDM.py
@@ -4,99 +4,22 @@ import numpy as np
 from scipy import linalg, allclose
 from profilehooks import profile
 
-from sample_Slater import sample_SlaterDeterminant, prob2cumul, bisection_search
-from test_suite import prepare_test_system_finiteT
+from sample_Slater import sample_SlaterDeterminant, sample_nonorthogonal_SlaterDeterminant, \
+                          prob2cumul, bisection_search, sample_expmX
+from test_suite import prepare_test_system_finiteT, square_region
+
+import itertools
 
 # REMOVE
 import matplotlib.pyplot as plt 
 # REMOVE
 
-# REMOVE
-def square_region(OBDM, L_A, x0=1, y0=1):
-    """
-        Extract the elements of the OBDM which correspond to a square 
-        region of the real-space lattice of size L_A x L_A.e 
-
-        The lower left corner of the square region has coordinates (1,1).
-    """
-    Ns = OBDM.shape[0]
-    L = int(np.sqrt(float(Ns)))
- 
-    row_idx = [(x0-1) + i + (y0-1)*L + (j-1)*L for j in range(1,L_A+1) for i in range(1,L_A+1)]
-    col_idx = row_idx 
-
-    return OBDM[np.ix_(row_idx, col_idx)]
-
-# REMOVE    
-
-
-def sample_FF_pseudo_density_matrix(OBDM, Nsamples):
-    """
-        PROTOTYPE: NOT ALL OPERATIONS ARE EFFICIENTLY IMPLEMENTED 
-        (NOT WORKING CORRECTLY FOR PSEUDO DENSITY MATRICES, 
-        gives the same results as sample_FF_OBDM())
-
-        Sample Fock states from a free fermion pseudo density matrix \rho,
-        which arises naturally in determinantal QMC simulations. 
-
-        \rho is non-hermitian and does not admit a spectral decomposition. 
-
-        The sampling of \rho proceeds in two steps. After calculating the distribution of 
-        particle numbers p[N] ("full counting statistics") of \rho, each direct sampling 
-        step consists of 
-          1. sampling a particle number sector of \rho according to p[N] => Np particles
-          2. sampling the sector of fixed particle number Np analogously to the way 
-             a single Slater determinant is sampled 
-            (see Yuan Wan's notes about free fermion pseudo density matrices).
-
-        Input: 
-            OBDM: Equal-time one-body density matrix.
-            Nsamples
-
-        Return type:
-            Generator object which outputs Nsamples occupation number states. 
-
-    """
-
-    assert( OBDM.shape[0] == OBDM.shape[1] ) 
-    Nsites = OBDM.shape[0] # number of sites = number of orbitals = max. total number of fermions 
-
-    # Calculate "full counting statistics"
-    p_N = particle_number_distribution(OBDM=OBDM)
-    # We will need the principal minors of the following matrix: 
-    expmX = linalg.inv( np.eye(Nsites) - OBDM ) - np.eye(Nsites)
-    # In the case of non-Hermitian X, we need the SVD of exp(-X).
-    U, singular_values, Vh = linalg.svd( expmX )
-
-    # partition sum 
-    print(U)
-    print(Vh)
-    print("singular_values=", singular_values)
-    print(linalg.det( np.eye(Nsites) + expmX ))
-    print(linalg.det( np.matmul(U.transpose(), Vh.transpose()) + np.diag(singular_values) )*linalg.det(U)*linalg.det(Vh) )
-    print( "sum(s[:])=", np.prod(singular_values) )
-    exit()
-
-    for ss in range(Nsamples):
-
-        # 1. Sample particle number 
-        Np = sample_particle_number_sectors(prob_N = p_N)
-
-        # 2. Sample the selected particle number sector of \rho. 
-        # Randomly pick an index set I of Np numbers from [0,1,...,Nsites].
-        I = np.sort(np.random.permutation(np.arange(Nsites))[0:Np])
-        nu_rndvec = np.random.permutation(np.arange(Np))
-
-        occ_vector = sample_SlaterDeterminant(U=expmX[:,I], nu_rndvec=nu_rndvec)
-        print('sample nr. =%d'%(ss), occ_vector)        
-        yield occ_vector
-
-
 
 def sample_FF_OBDM(OBDM, Nsamples_per_mode_occ, Nsamples_per_Sdet):
     """
         Sample Fock states from a spinless free fermion (FF) one-body density matrix (OBDM).
-        In general, the OBDM is not hermitian. 
+        In general, the OBDM is not hermitian, in which case this subroutine gives 
+        wrong results.
 
         Return type:
             Generator object which outputs occupation number states. 
@@ -180,6 +103,53 @@ def sample_FF_OBDM(OBDM, Nsamples_per_mode_occ, Nsamples_per_Sdet):
     print("========= CHECK ===================")
 
 
+def sample_FF_pseudo_density_matrix(OBDM, Nsamples):
+    """
+        Sample Fock states from a free fermion pseudo density matrix \rho,
+        which arises naturally in determinantal QMC simulations. 
+        \rho is non-hermitian and does not admit a spectral decomposition. 
+
+        The sampling of \rho proceeds in two steps:
+            1. A particle number sector is sampled. 
+            2. In the sector of N particles, N particles positions are sampled 
+               using a chain of conditional probabilities, which are given 
+               as ratios of principal minors of exp(-X)
+        Since for a pseudo density matrix these principal minors can be negative,
+        each generated occupation number state is output together with its sign. 
+
+        (see Yuan Wan's notes about free fermion pseudo density matrices).
+
+        Input: 
+            OBDM: Equal-time one-body density matrix.
+            Nsamples
+
+        Return type:
+            Generator object which outputs Nsamples occupation number states. 
+
+    """
+
+    assert( OBDM.shape[0] == OBDM.shape[1] ) 
+    Nsites = OBDM.shape[0] # number of sites = number of orbitals = max. total number of fermions 
+
+    # Calculate the "full counting statistics", i.e. the particle number distribution
+    p_N = particle_number_distribution(OBDM=OBDM)
+  
+    # The conditional probabilities are ratios of the principal minors of the 
+    # following matrix: 
+    expmX = linalg.inv( np.eye(Nsites) - OBDM ) - np.eye(Nsites)
+
+    for ss in range(Nsamples):
+
+        # 1. Sample particle number 
+        Np = sample_particle_number_sectors(prob_N = p_N)
+
+        # 2. Sample the Np-particle reduced density matrix. 
+        occ_vector, sign_vector = sample_expmX(expmX, Np)
+        print('sample nr. =%d'%(ss), occ_vector, sign_vector, "prod(sign_vec)=%d" % (np.prod(sign_vector[abs(sign_vector)>0])))
+
+        yield occ_vector, sign_vector
+
+
 def particle_number_distribution(OBDM):
     """
         Compute the full counting statistics of the particle number given a 
@@ -229,6 +199,27 @@ def sample_particle_number_sectors(prob_N):
     return N 
 
 
+def sample_index_setK(singular_values):
+    """
+        Convert the singular values (which are by construction non-negative real numbers)
+        into a normalized discrete probability distribution prob[k].
+        Include the index k with probabilite prob[k] in the index set. 
+
+        Input:
+            Array of singular vectors.
+        Output:
+            Returns a sorted index set. 
+    """
+    assert( len(singular_values.shape) == 1 )
+    singular_values = np.array(singular_values)
+    N = len(singular_values)
+
+    prob = singular_values / sum(singular_values)
+    index_set = np.where(np.random.random(size=N) < prob)[0]
+
+    return index_set
+
+
 def principal_minor(A, index_set):
     """
         Compute the principal minor ("inclusive" definition)
@@ -250,6 +241,7 @@ def principal_minor(A, index_set):
 def all_principal_minors(A):
     """
         Return all principal minors of a square matrix A.
+        Return type: Generator object.
     """
     A = np.array(A)
     assert( A.shape[0] == A.shape[1] ); N = A.shape[0]
@@ -261,81 +253,191 @@ def all_principal_minors(A):
                 yield  principal_minor(A, index_set)
 
 
+def constrained_sum_of_principal_kxk_minors(A, k, Ic):
+    """
+        Compute the sum of all k-by-k principal minors ("inclusive definition")
+            \sum_{I} [A]_{I,I}
+        of the D-by-D matrix A under the constraint that the 
+        ordered index set I (of cardinality k) contains 
+        the index set Ic \subset {0,1,2,...,D-1}.
+
+        Note: The sum is over \binom{D,k} terms, which is only 
+              feasible for small D and k. 
+    """
+
+    A = np.array(A); Ic = set(np.array([Ic]).flatten())
+    assert(len(A.shape)==2)
+    assert(A.shape[0] == A.shape[1])
+    D = A.shape[0]
+    assert(0 < k <= D)
+    assert(Ic <= set(np.arange(D)))
+    
+    csum = 0.0
+    for comb in itertools.combinations(set(np.arange(D)), k):
+        S = set(comb)
+        # Only sum over index sets S which contain Ic as a subset. 
+        if (Ic <= S):
+            I = np.sort(tuple(S))
+            csum += principal_minor(A, I)
+    return csum
+
+
+def sample_FF_pseudo_density_matrix_prototype(OBDM, Nsamples):
+    """
+        PROTOTYPE: NOT ALL OPERATIONS ARE EFFICIENTLY IMPLEMENTED 
+        (NOT WORKING CORRECTLY FOR PSEUDO DENSITY MATRICES, 
+        gives the same results as sample_FF_OBDM())
+
+        Sample Fock states from a free fermion pseudo density matrix \rho,
+        which arises naturally in determinantal QMC simulations. 
+
+        \rho is non-hermitian and does not admit a spectral decomposition. 
+
+        The sampling of \rho proceeds in two steps. After calculating the distribution of 
+        particle numbers p[N] ("full counting statistics") of \rho, each direct sampling 
+        step consists of 
+          1. sampling a particle number sector of \rho according to p[N] => Np particles
+          2. sampling the sector of fixed particle number Np analogously to the way 
+             a single Slater determinant is sampled 
+            (see Yuan Wan's notes about free fermion pseudo density matrices).
+
+        Input: 
+            OBDM: Equal-time one-body density matrix.
+            Nsamples
+
+        Return type:
+            Generator object which outputs Nsamples occupation number states. 
+
+    """
+
+    assert( OBDM.shape[0] == OBDM.shape[1] ) 
+    Nsites = OBDM.shape[0] # number of sites = number of orbitals = max. total number of fermions 
+
+    # Calculate "full counting statistics"
+    p_N = particle_number_distribution(OBDM=OBDM)
+
+    # We will need the principal minors of the following matrix: 
+    expmX = linalg.inv( np.eye(Nsites) - OBDM ) - np.eye(Nsites)
+
+    for ss in range(Nsamples):
+
+        # 1. Sample particle number 
+        Np = sample_particle_number_sectors(prob_N = p_N)
+
+        # 2. Sample the selected particle number sector of \rho. 
+        # Randomly pick an index set I of Np numbers from [0,1,...,Nsites].
+        I = np.sort(np.random.permutation(np.arange(Nsites))[0:Np])
+        nu_rndvec = np.random.permutation(np.arange(Np))
+
+        occ_vector = sample_SlaterDeterminant(U=expmX[:,I], nu_rndvec=nu_rndvec)
+        print('sample nr. =%d'%(ss), occ_vector)        
+        yield occ_vector
+
+
 if __name__ == '__main__':
 
     # =============================
     #  OBDM from determinantal QMC
     # =============================
     OBDM_tot = np.loadtxt("../test_data/OBDM_L16x16_U7.2mu3.6_attractiveHubbard.dat")
-    # OBDM = OBDM_tot[np.ix_(np.arange(Ns//2,3*Ns//4), np.arange(Ns//2,3*Ns//4))]
+    #OBDM = OBDM_tot[np.ix_(np.arange(Ns//2,3*Ns//4), np.arange(Ns//2,3*Ns//4))]
     #OBDM = OBDM_tot[np.ix_([102,103],[102,103])]
     #np.savetxt("questionable_OBDM.dat", OBDM)
-    OBDM = square_region(OBDM_tot, L_A=4, x0=5, y0=5)
+    OBDM = square_region(OBDM_tot, L_A=4, x0=1, y0=3)
+    #print("norm pseudo FF=", sum(np.diag(OBDM)))
     assert( OBDM.shape[0] == OBDM.shape[1] )
     Nsites = OBDM.shape[0]
     beta=8.0
     mu=3.6
 
+    NsModes = -10
+
     # p_N = particle_number_distribution(OBDM=OBDM)
-    # plt.plot(np.arange(len(p_N)), p_N)
-    # plt.show()
+    # # plt.plot(np.arange(len(p_N)), p_N)
+    # # plt.show()
 
     # # Are all principal minors of exp(-X) positive ? 
     # # =================================================================
     # expmX = linalg.inv( np.eye(Nsites) - OBDM ) - np.eye(Nsites)
 
-    # for minor_val in all_principal_minors(expmX):
-    #     print(minor_val)
+    # # for minor_val in all_principal_minors(expmX):
+    # #     print(minor_val)
 
+    # summe = 0.0
+    # for i in np.arange(Nsites):
+    #     Z = constrained_sum_of_principal_kxk_minors(expmX, 3, [0])
+    #     prob = constrained_sum_of_principal_kxk_minors(expmX, 3, [i,0]) / Z
+    #     summe += prob
+    #     print(summe, prob)
 
-#  #   ## =======================================
-#  #   ##  Thermal OBDM for free fermion system
-#  #   ## ========================================
-#  #   Nsites, beta, mu, MDF, OBDM = prepare_test_system_finiteT(Nsites=81, beta=5.00, mu=0.0, potential='random-binary')
+   ## =======================================
+   ##  Thermal OBDM for free fermion system
+   ## ========================================
+    # Nsites, beta, mu, MDF, OBDM = prepare_test_system_finiteT(Nsites=7, beta=10.0, mu=0.0, potential='random-binary')
 
-#     NsModes=10  # How often are the natural orbitals of the many-body density matrix sampled to construct a Slater determinant ? 
-#     NsDet=10     # How often is each Slater determinant sampled ? 
-  
-# #    # generator function 
-# #    generate_Fock_states = sample_FF_OBDM(OBDM, Nsamples_per_mode_occ=NsModes, Nsamples_per_Sdet=NsDet)
+# # #    # generator function 
+    NsModes=5000  # How often are the natural orbitals of the many-body density matrix sampled to construct a Slater determinant ? 
+    NsDet=1     # How often is each Slater determinant sampled ? 
+    #generate_Fock_states = sample_FF_OBDM(OBDM, Nsamples_per_mode_occ=NsModes, Nsamples_per_Sdet=NsDet)
 
-    generate_Fock_states = sample_FF_pseudo_density_matrix(OBDM=OBDM, Nsamples=1000)
+    generate_Fock_states = sample_FF_pseudo_density_matrix(OBDM=OBDM, Nsamples=10000)
  
     av_density = np.zeros(Nsites)
+    av_density_signcorrected = np.zeros(Nsites)
     av2_density = np.zeros(Nsites)
+    av_sign = np.zeros(Nsites)
+    av2_sign = np.zeros(Nsites)
 
     ss = 0
-    for occ_vector in generate_Fock_states:
+    for occ_vector, sign_vector in generate_Fock_states:
         ss += 1
         #print('sample nr. =%d'%(ss), occ_vector)
         av_density += occ_vector
+        av_density_signcorrected += occ_vector * np.prod(sign_vector[abs(sign_vector)>0])
         av2_density += occ_vector**2
+        av_sign += sign_vector
+        av2_sign += sign_vector**2
     Nsamples = ss
+    av_sign /= av_density   # divide by the number of occurences of the particular sampled position 
+    av2_sign /= av_density
     av_density /= float(Nsamples)
+    av_density_signcorrected /= float(Nsamples)
     av2_density /= float(Nsamples)
+    
     sigma_density = np.sqrt(av2_density - av_density**2) / np.sqrt(Nsamples)
+    sigma_sign = np.sqrt(av2_sign - av_sign**2) / np.sqrt(Nsamples)
     print("sum_av_density=", sum(av_density))
     print("sum(np.diag(OBDM))=", sum(np.diag(OBDM)))
     print(sigma_density)     
     
-# #    # plot
-#     plt.errorbar(np.arange(Nsites), av_density, yerr=sigma_density, fmt='-o', \
-#             label='sampling Slater determinant \n Nsamples = %d' % (Nsamples) \
-#                    + ' \n NsModes = %d' % (NsModes))
-#     plt.plot(np.arange(Nsites), np.diag(OBDM), label='from Green\'s function')
-#     plt.xlabel('site i')
-# #    # plt.title('Free fermions in a parabolic trap.\n'+r'Nsites=%d, $\beta$=%4.2f, $\mu$=%4.2f' %(Nsites,beta,mu))
-# #    plt.title('Free fermions in a random binary potential.\n'+r'Nsites=%d, $\beta$=%4.2f, $\mu$=%4.2f' %(Nsites,beta,mu))
-#     plt.ylabel(r'$\langle \hat{n}_i \rangle$')
-#     plt.legend(loc='lower center')
-# #    plt.show()
-# #    plt.savefig('FF_trap_sample_FF_OBDM_L%d_beta%4.2f_NperModes%d_NperDet%d.png' \
-# #               % (Nsites, beta, NsModes, NsDet))
+#    # plot
 
-#     # =============================
-#     #  OBDM from determinantal QMC
-#     # =============================   
-#     plt.title('HS sample from DQMC simulations.\n'+r'Nsites=%d, $\beta$=%4.2f, $\mu$=%4.2f' %(Nsites,beta,mu))            
-#     plt.savefig('HSsample_U-7.2mu3.6beta8_L%d_OBDM_LA4_NperModes%d_NperDet%d.png' \
-#                % (Nsites, NsModes, NsDet))               
-#     plt.show()
+    fig, (ax1, ax2) = plt.subplots(2,1)
+
+    ax1.errorbar(np.arange(Nsites), av_density, yerr=sigma_density, fmt='-+', \
+            label='average density (ignoring sign)')
+    ax1.errorbar(np.arange(Nsites), av_density_signcorrected, yerr=sigma_density, fmt='-', \
+            label='average density (sign-corrected)')            
+    ax1.plot(np.arange(Nsites), np.diag(OBDM), label='from Green\'s function')
+    #ax1.xlabel('site i')
+#    # plt.title('Free fermions in a parabolic trap.\n'+r'Nsites=%d, $\beta$=%4.2f, $\mu$=%4.2f' %(Nsites,beta,mu))
+#    plt.title('Free fermions in a random binary potential.\n'+r'Nsites=%d, $\beta$=%4.2f, $\mu$=%4.2f' %(Nsites,beta,mu))
+    #ax1.ylabel(r'$\langle \hat{n}_i \rangle$')
+    ax1.legend(loc='lower center')
+
+    ax2.errorbar(np.arange(Nsites), av_sign, yerr=sigma_sign, fmt='-o', \
+            label=r'average sign (whenever site i is result of a sampling step)')
+    #ax2.xlabel('site i')
+    #ax2.ylabel(r'average sign')
+    plt.show()
+
+#    plt.savefig('FF_trap_sample_FF_OBDM_L%d_beta%4.2f_NperModes%d_NperDet%d.png' \
+#               % (Nsites, beta, NsModes, NsDet))
+
+# #     # =============================
+# #     #  OBDM from determinantal QMC
+# #     # =============================   
+# #     plt.title('HS sample from DQMC simulations.\n'+r'Nsites=%d, $\beta$=%4.2f, $\mu$=%4.2f' %(Nsites,beta,mu))            
+# #     plt.savefig('HSsample_U-7.2mu3.6beta8_L%d_OBDM_LA4_NperModes%d_NperDet%d.png' \
+# #                % (Nsites, NsModes, NsDet))               
+# #     plt.show()

test_suite.py
@@ -40,14 +40,14 @@ def prepare_test_system_finiteT(Nsites=21, beta=1.0, mu=0.0, potential='paraboli
             beta = inverse temperature 
             mu = chemical potential 
             potential: Type of the external potenial which is either 'parabolic'
-               of 'random-binary. 
+               or 'random-binary. 
         Output:
             Return the one-body density matrix (OBDM)
                 <c_i^{\dagger} c_j> = Tr(e^{-beta H}c_i^{\dagger} c_j)
             and the occupations of natural orbitals (momentum-distribution
             function for a translationally-invariant system) as a vector.
     """
-    assert(Nsites%2==1), "test_suites: Nsites should be an odd number."
+    #assert(Nsites%2==1), "test_suites: Nsites should be an odd number."
 
     i0=int(Nsites/2)
     V = np.zeros(Nsites)
@@ -84,3 +84,19 @@ def prepare_test_system_finiteT(Nsites=21, beta=1.0, mu=0.0, potential='paraboli
     OBDM = np.matmul(np.matmul(U, MDF), U.conj().T)
 
     return (Nsites, beta, mu, np.sort(np.diag(MDF))[::-1], OBDM)
+
+
+def square_region(OBDM, L_A, x0=1, y0=1):
+    """
+        Extract the elements of the OBDM which correspond to a square 
+        region of the real-space lattice of size L_A x L_A.e 
+
+        The lower left corner of the square region has coordinates (1,1).
+    """
+    Ns = OBDM.shape[0]
+    L = int(np.sqrt(float(Ns)))
+ 
+    row_idx = [(x0-1) + i + (y0-1)*L + (j-1)*L for j in range(1,L_A+1) for i in range(1,L_A+1)]
+    col_idx = row_idx 
+
+    return OBDM[np.ix_(row_idx, col_idx)]